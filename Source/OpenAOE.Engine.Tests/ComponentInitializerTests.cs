e171d0b21411e1a269f44114e1a4aaa33b8239a6
Simon Moles <simon.moles@gmail.com>
2016-01-07 17:35:42 +0000

    Added tests for component initialiser.

---
+++ Source/OpenAOE.Engine.Tests/ComponentInitializerTests.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using NUnit.Framework;
+using OpenAOE.Engine.Data;
+using OpenAOE.Engine.Tests.TestData.Components;
+using Shouldly;
+
+namespace OpenAOE.Engine.Tests
+{
+    [TestFixture]
+    public class ComponentInitializerTests
+    {
+        class InvalidComponentInterface : Component<InvalidComponentInterface, IComponent, IWriteableSimpleComponent>,
+            IWriteableSimpleComponent
+        {
+            public override void CopyTo(InvalidComponentInterface other)
+            {
+                throw new global::System.NotImplementedException();
+            }
+
+            public int Value
+            {
+                set { throw new global::System.NotImplementedException(); }
+            }
+        }
+
+        class InvalidWriteableComponentInterface : Component<InvalidWriteableComponentInterface, ISimpleComponent, IWriteableComponent>,
+            ISimpleComponent, IWriteableComponent
+        {
+            public override void CopyTo(InvalidWriteableComponentInterface other)
+            {
+                throw new NotImplementedException();
+            }
+
+            public int Value
+            {
+                get
+                {
+                    throw new NotImplementedException();
+                }
+            }
+        }
+
+        public void EnsureStaticInitalizerThrowsForInvalidComponentInterface()
+        {
+            Should.Throw<TypeInitializationException>(() =>
+            {
+                new InvalidComponentInterface();
+            });
+        }
+
+        public void EnsureStaticInitalizerThrowsForInvalidWriteableComponentInterface()
+        {
+            Should.Throw<TypeInitializationException>(() =>
+            {
+                new InvalidWriteableComponentInterface();
+            });
+        }
+
+        public void EnsureStaticInitializerDoesNotThrowForValidType()
+        {
+            // TODO: This wouldn't throw anyway if SimpleComponent has already been used in another unit test this session.
+            // But, it would fail that unit test so it should be fine...
+            Should.NotThrow(() => new SimpleComponent());
+        }
+    }
+}
