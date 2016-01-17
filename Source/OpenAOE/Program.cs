dbd70ed7af3e197073534b0a2c18c54011979c7c
Simon Moles <simon.moles@gmail.com>
2016-01-17 11:59:48 +0000

    Update default project to start an engine and tick a few times.

--- Source/OpenAOE/Program.cs
+++ Source/OpenAOE/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
 using Ninject;
 using Ninject.Extensions.Logging;
 using Ninject.Extensions.Logging.NLog4;
 using OpenAOE.Engine;
+using OpenAOE.Engine.Entity;
 
 namespace OpenAOE
 {
@@ -14,10 +16,24 @@ namespace OpenAOE
                 new NinjectSettings()
                 {
                     LoadExtensions = false
-                }, new EngineModule(), new Games.AGE2.Module(), new NLogModule());
+                }, new Engine.EngineModule(), new Games.AGE2.Module(), new NLogModule());
+
 
             var log = context.Get<ILoggerFactory>().GetCurrentClassLogger();
             log.Info("Starting");
+
+            var engineFactory = context.Get<IEngineFactory>();
+
+            log.Info("Creating Engine");
+            var engine = engineFactory.Create(new List<EntityData>(), new List<EntityTemplate>());
+
+            for (var i = 0; i < 10; ++i)
+            {
+                var tick = engine.Tick(new EngineTickInput());
+                tick.Start();
+                tick.Wait();
+                engine.Synchronize();
+            }
 
             /*context.Bind<IDataService>().ToConstant(VersionedDataService.FromRoot(new Root()));
             var instance = context.Get<ISimulationInstance>();
