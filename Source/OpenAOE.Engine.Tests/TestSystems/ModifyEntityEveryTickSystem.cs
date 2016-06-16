2ca93768683a561f5ca58d93afec29af0c981477
Simon Moles <simon.moles@gmail.com>
2016-06-16 14:15:04 +0100

    Fixed compiler warnings.

--- Source/OpenAOE.Engine.Tests/TestSystems/ModifyEntityEveryTickSystem.cs
+++ Source/OpenAOE.Engine.Tests/TestSystems/ModifyEntityEveryTickSystem.cs
@@ -6,7 +6,7 @@ namespace OpenAOE.Engine.Tests.TestSystems
 {
     public class ModifyEntityEveryTickSystem : FilteredSystem<ISimpleComponent>, Triggers.IOnEntityTick
     {
-        public string Name => nameof(ModifyEntityEveryTickSystem);
+        public override string Name => nameof(ModifyEntityEveryTickSystem);
 
         public void OnTick(EngineEntity entity)
         {
